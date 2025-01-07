3425b19452112029717859811037b42648828900
Remo Inverardi <remo@inverardi.ch>
2025-01-07 23:31:12 +0100

    Initialize the Next.js app.

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Tschain Sepp",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1>Tschain Sepp</h1>;
+};
+
+export default Page;
