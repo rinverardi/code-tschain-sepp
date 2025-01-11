c5a3755c604c0e0d7d1067afba93eeafbd1bd0ce
Remo Inverardi <remo@inverardi.ch>
2025-01-11 23:40:46 +0100

    Watch games for changes.

--- components/game_account.tsx
+++ components/game_account.tsx
@@ -1,24 +1,46 @@
 import { IdlAccounts, Program } from "@coral-xyz/anchor";
-import { PublicKey } from "@solana/web3.js";
+import { useEffect } from "react";
+import { Connection, PublicKey } from "@solana/web3.js";
 
 import { TschainSepp } from "../target/types/tschain_sepp";
 
 type GameAccount = IdlAccounts<TschainSepp>["game"];
 
-export async function fetchGame(
-  program: Program<TschainSepp>,
-  id: string
-): Promise<GameAccount> {
-  const address = findAddress(program, id);
-
-  return await program.account.game.fetch(address);
-}
-
-function findAddress(program: Program<TschainSepp>, id: string): PublicKey {
+export function deriveAddress(program: Program<TschainSepp>, id: string): PublicKey {
   const [address, _] = PublicKey.findProgramAddressSync(
     [Buffer.from("game"), Buffer.from(id)],
     program.programId
   );
 
   return address;
+}
+
+export async function fetchGame(
+  address: PublicKey,
+  program: Program<TschainSepp>,
+): Promise<GameAccount> {
+  return await program.account.game.fetch(address);
+}
+
+export function watchGame(
+  address: PublicKey,
+  connection: Connection,
+  program: Program<TschainSepp>,
+  listener: (account: GameAccount) => void,
+): void {
+  useEffect(() => {
+    const subscription = connection.onAccountChange(
+      address,
+      (info) => {
+        const game = program.coder.accounts.decode<GameAccount>("game", info.data);
+
+        listener(game);
+      },
+      { commitment: "confirmed" },
+    );
+
+    return () => {
+      connection.removeAccountChangeListener(subscription);
+    }
+  });
 }
